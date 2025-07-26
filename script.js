88674144493ab749b7e744b5c85fbf788f5b7725
Kevon <seni0026@algonquinlive.com>
2025-07-26 01:28:20 -0400

    Make Load More Button Interactive Make the load more button interactive by fetching the next 20 Pokemon when clicked

--- script.js
+++ script.js
@@ -17,37 +17,57 @@ const response = '';
 const pokeApiJson = '';
 const firstTwentyPokeLink = 'https://pokeapi.co/api/v2/pokemon';
 const newPokeInfo = '';
+let nextLink = '';
+let pokeUrl = '';
 
-fetchPokeData();
+// call function to display pokemon
+pokemonDisplay();
 
-// function to for initial API call
-async function fetchPokeData() {
-    // get the return value from the fetchData()
-    const pokeInfo = fetchData(response, pokeApiJson, firstTwentyPokeLink);
+// call function to load more pokemon when button is click
+// loadPokemon();
+
+// function for initial API call
+async function pokemonDisplay() {
+
+    const pokeJson = await initialJsonObj(firstTwentyPokeLink);
+    nextLink = pokeJson.next;
+    loadPokemon(nextLink)
+
+    getPokeSprite(pokeJson, newPokeInfo, pokeUrl);
+}
+
+// function for initial API call
+async function initialJsonObj(apiLink) {
+        // get the return value from the fetchData()
+    const pokeInfo = fetchData(response, pokeApiJson, apiLink);
 
     // get clarity from professor on why i need await to get the value here
     const pokeJson = await pokeInfo;
-    // nextLink = pokeJson.next;
-
-    // console.log(nextLink)
-
-    getPokeSprite(pokeJson, firstTwentyPokeLink, newPokeInfo)
+    return pokeJson;
 }
 
 // this function get pokemon sprites
-async function getPokeSprite(json, api, sprite) {
-        for(let i = 0; i < 20; i++) {
-        const pokeName = json.results[i].name;
-        let pokeSpriteLink = `${api}/${pokeName}`;
-            sprite = fetchData(response, pokeApiJson, pokeSpriteLink);
-            const spriteResponse = await sprite;
-            pokeSprite = spriteResponse.sprites.other["official-artwork"].front_default;
+async function getPokeSprite(json, sprite, url) {
+    // use a for loop to loop through the results array 
+    // and get the url value from each object in the array
+    for(let i = 0; i < 20; i++) {
+        url = json.results[i].url;
+
+        // fetch the data for specific pokemon
+        sprite = fetchData(response, pokeApiJson, url);
+        const spriteResponse = await sprite;
+
+        // access the sprites object and save a sprite to a variable
+        pokeSprite = spriteResponse.sprites.other["official-artwork"].front_default;
+
+        // create the html template to display pokemon
         thumbnailHTML = `
             <div class="thumbnail">
                 <img src="${pokeSprite}" alt="">
-                <p class="pokemon-name">${pokeName}</p>
+                <p class="pokemon-name">${spriteResponse.name}</p>
             </div>`
 
+        // add the template to the page
         pokeDisplay.innerHTML+= thumbnailHTML;
     }
 }
@@ -58,4 +78,24 @@ async function fetchData(response, json, apiLink) {
     json = await response.json();
     return json
     // console.log(json)
+}
+
+// this function load more pokemon to the page
+function loadPokemon(next) {
+    loadPokemonBtn.addEventListener('click', async function() {
+        
+        const pokeJson = await initialJsonObj(next)
+        // console.log(pokeJson.results)
+        getPokeSprite(pokeJson, newPokeInfo, next);
+    })
+}
+
+Obj(firstTwentyPokeLink);
+async function Obj(apiLink) {
+        // get the return value from the fetchData()
+    const pokeInfo = fetchData(response, pokeApiJson, "https://pokeapi.co/api/v2/pokemon?offset=40&limit=20");
+
+    // get clarity from professor on why i need await to get the value here
+    const pokeJson = await pokeInfo;
+    console.log(pokeJson);
 }
