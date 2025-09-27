02c20b450e9a35989e5670e20907b10672491f51
Kevon <seni0026@algonquinlive.com>
2025-09-27 15:35:49 -0400

    Revert "Merge pull request #4 from seniork7/part-3"

--- script.js
+++ script.js
@@ -186,8 +186,8 @@ async function getPokeSprite(json, url) {
 
         console.log(pokeApiJson)
         for(let j = 0; j < 20; j++) {
-            pokeAttacks.pish(pokeApiJson.moves[j].move.name);
-        }
+            pokeAttacks.push(pokeApiJson.moves[j].move.name);
+        }   
 
         console.log(pokeAttacks)
 
